9b46d0fc9a1487dd0489036de3eaf6f6a1ab57a2
Midhun-Waywedesign <midhun.km@waywedesign.com>
2025-10-06 11:19:54 +0530

    production issue solved3

--- redux/baseUrl.ts
+++ redux/baseUrl.ts
@@ -1,1 +1,4 @@
-export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL!; // Ensure this is defined in your environment variables
+// If NEXT_PUBLIC_BASE_URL is not set in production, use empty string
+export const BASE_URL =
+  process.env.NEXT_PUBLIC_BASE_URL ||
+  (process.env.NODE_ENV === "production" ? "" : "http://localhost:3001");
