8ba6522e16db7b9efa62e0455c56084dee2052e2
Sreerag-K-P <waywedesign@gmail.com>
2025-10-16 17:11:23 +0530

    push datas

--- components/student/SwitchBatchDialog.tsx
+++ components/student/SwitchBatchDialog.tsx
@@ -44,7 +44,7 @@ const SwitchBatchDialog: React.FC<SwitchBatchDialogProps> = ({
 
   return (
     <Dialog open={open} onOpenChange={onOpenChange}>
-      <DialogContent className="max-w-2xl">
+      <DialogContent className="max-w-2xl bg-blue-100/10 backdrop-blur-md text-white border-none shadow-inner">
         <DialogHeader>
           <DialogTitle>Switch Student Batch</DialogTitle>
           <DialogDescription>
