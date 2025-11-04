d55269171889c1ef207dc928f33804c30a3bbacd
Sreerag-K-P <waywedesign@gmail.com>
2025-11-04 09:53:36 +0530

    edit page unchnged

--- components/Users/EditUserForm.tsx
+++ components/Users/EditUserForm.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useAppSelector, useAppDispatch } from "@/lib/hooks";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
@@ -99,7 +99,6 @@ const EditUserForm: React.FC<EditUserFormProps> = ({
           value={locationId}
           onValueChange={setLocationId}
           disabled={usersLoading}
-          required
         >
           <SelectTrigger className="w-full">
             <SelectValue placeholder="Select location" />
