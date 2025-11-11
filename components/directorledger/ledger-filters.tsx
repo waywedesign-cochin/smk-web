b65f883a2458cd801922b48fd5a20357b4632896
Sreerag-K-P <waywedesign@gmail.com>
2025-11-11 14:18:26 +0530

    ledger filet style fixes

--- components/directorledger/ledger-filters.tsx
+++ components/directorledger/ledger-filters.tsx
@@ -174,8 +174,8 @@ export function LedgerFilters({
         <div
           className={
             showDirectorFilter
-              ? "md:col-span-2 relative "
-              : "md:col-span-2 relative "
+              ? "md:col-span-2 relative   "
+              : " relative md:col-span-2 space-y-2 mt-auto "
           }
         >
           <Search className="absolute left-3 top-1/2 h-4 w-4 -translate-y-1/2 text-gray-300" />
